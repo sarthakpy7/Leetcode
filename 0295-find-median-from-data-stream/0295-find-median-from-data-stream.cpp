c9f6d3866243f64617952fd48d1a9642325afc21
sarthakpy7 <128068382+sarthakpy7@users.noreply.github.com>
2024-09-17 16:45:37 +0530

    Time: 276 ms (33.44%), Space: 121.9 MB (81.02%) - LeetHub

--- 0295-find-median-from-data-stream/0295-find-median-from-data-stream.cpp
+++ 0295-find-median-from-data-stream/0295-find-median-from-data-stream.cpp
@@ -1,44 +1,28 @@
-#include <bits/stdc++.h>
+#include <queue>
 using namespace std;
 
 class MedianFinder {
 public:
-    multiset<int> low, high;  
+    priority_queue<int> maxHeap;  
+    priority_queue<int, vector<int>, greater<int>> minHeap; 
+
     MedianFinder() {
     }
+
     void addNum(int num) {
-        if (low.empty() || num <= *low.rbegin()) {
-            low.insert(num);
-        } else {
-            high.insert(num);
-        }
-        balance();
-    }
-    void balance() {
-        if (low.size() < high.size()) {
-            int x = *high.begin();
-            high.erase(high.find(x));
-            low.insert(x);
-        }
-        if (low.size() > high.size() + 1) {
-            int x = *low.rbegin();
-            low.erase(low.find(x));
-            high.insert(x);
+        maxHeap.push(num);
+        minHeap.push(maxHeap.top());
+        maxHeap.pop();
+        if (maxHeap.size() < minHeap.size()) {
+            maxHeap.push(minHeap.top());
+            minHeap.pop();
         }
     }
+
     double findMedian() {
-        if ((low.size() + high.size()) % 2 == 1) {
-            return *low.rbegin();  
-        } else {
-            return (*low.rbegin() + *high.begin()) / 2.0;  
+        if (maxHeap.size() > minHeap.size()) {
+            return maxHeap.top();
         }
+        return (maxHeap.top() + minHeap.top()) / 2.0;
     }
 };
-
-
-/**
- * Your MedianFinder object will be instantiated and called as such:
- * MedianFinder* obj = new MedianFinder();
- * obj->addNum(num);
- * double param_2 = obj->findMedian();
- */
