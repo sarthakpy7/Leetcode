a56321b129ee8a72f4b45b07541bd21d586e0ff8
sarthakpy7 <128068382+sarthakpy7@users.noreply.github.com>
2024-12-10 11:14:17 +0530

    Time: 7 ms (99.02%), Space: 79.2 MB (83%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
@@ -1,36 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
 class Solution {
 public:
     int largestRectangleArea(vector<int>& heights) {
-        int n = heights.size();
-        vector<int> psm(n); // Previous smaller element index
-        vector<int> nsm(n); // Next smaller element index
-        
-        // Compute psm (Previous Smaller Element)
-        for (int i = 0; i < n; i++) {
-            int val = i - 1;
-            while (val != -1 && heights[val] >= heights[i]) {
-                val = psm[val];
-            }
-            psm[i] = val;
-        }
-
-        // Compute nsm (Next Smaller Element)
-        for (int i = n - 1; i >= 0; i--) {
-            int val = i + 1;
-            while (val != n && heights[val] >= heights[i]) {
+        int n=heights.size();
+        int nsm[n];
+         for(int i=n-1;i>=0;i--){
+            int val=i+1;
+            while(val!=n && heights[val] >= heights[i]){
                 val = nsm[val];
             }
-            nsm[i] = val;
-        }
+            nsm[i]=val;
+         }
+        int psm[n];
+        for(int i=0;i<n;i++){
+            int val=i-1;
+            while(val!=-1 && heights[val] >= heights[i]){
+                val = psm[val];
+            }
+            psm[i]=val;
+         }
 
-        // Calculate the maximum area
-        long long area = 0;
-        for (int i = 0; i < n; i++) {
-            long long height = heights[i];
-            long long width = nsm[i] - psm[i] - 1;
-            area = max(area, height * width);
-        }
+         int area=0;
+         for(int i=0;i<n;i++){
+            int height = heights[i];
+            int width = nsm[i] - psm[i] - 1;
+            area = max(area, height*width);
+         }
+         return area;
 
-        return area;
+
     }
 };
