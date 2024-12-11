e305f93a75a321802f4c5c18db87b7403d6809d9
sarthakpy7 <128068382+sarthakpy7@users.noreply.github.com>
2024-12-11 10:58:28 +0530

    Time: 128 ms (37.46%), Space: 111.6 MB (44.44%) - LeetHub

--- 2891-maximum-beauty-of-an-array-after-applying-operation/2891-maximum-beauty-of-an-array-after-applying-operation.cpp
+++ 2891-maximum-beauty-of-an-array-after-applying-operation/2891-maximum-beauty-of-an-array-after-applying-operation.cpp
@@ -1,22 +1,36 @@
 class Solution {
 public:
-    static int maximumBeauty(vector<int>& nums, int k) {
-        int freq[100001]={0};
-        int xMax=0, xMin=1e6;
-        for(int x: nums){
-            freq[x]++;
-            xMax=max(x, xMax);
-            xMin=min(x, xMin);
-        }
-        int cnt=0, maxCnt=0;
-        for(int l=xMin, r=xMin; r<=xMax; r++){
-            cnt+=freq[r];
-            while(r-l>2*k){
-                cnt-=freq[l];
-                l++;
+    int maximumBeauty(vector<int>& nums, int k) {
+        int n = nums.size();
+        int maxValue = 0;
+
+        // Find the maximum value in the array
+        for (int i = 0; i < n; i++) {
+            if (nums[i] > maxValue) {
+                maxValue = nums[i];
             }
-            maxCnt=max(maxCnt, cnt);
         }
-        return maxCnt;
+
+        // Create an array to track ranges
+        vector<int> range(maxValue + 10, 0);
+
+        // Mark ranges for each number in the array
+        for (int i = 0; i < n; i++) {
+            int left = max(0, nums[i] - k);
+            int right = min(maxValue, nums[i] + k) + 1;
+            range[left]++;
+            range[right]--;
+        }
+
+        // Calculate prefix sums and find the maximum value
+        int result = range[0];
+        for (int i = 1; i < range.size(); i++) {
+            range[i] += range[i - 1];
+            if (range[i] > result) {
+                result = range[i];
+            }
+        }
+
+        return result;
     }
 };
