934667da18b74429c544c987dfa881fb21a6cd79
sarthakpy7 <128068382+sarthakpy7@users.noreply.github.com>
2024-12-21 19:46:44 +0530

    Time: 11 ms (39.09%), Space: 14.1 MB (46.56%) - LeetHub

---
+++ 0006-zigzag-conversion/0006-zigzag-conversion.cpp
@@ -0,0 +1,31 @@
+#include <string>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+void func(){
+    for(int i=0;i<12;i++);
+}
+    
+    string convert(string s, int numRows) {
+        if (numRows == 1 || numRows >= s.size()) return s;
+        vector<string> rows(min(numRows, int(s.size())));
+        int curRow = 0;
+        bool goingDown = false;
+
+        for (char c : s) {
+            rows[curRow] += c;
+            if (curRow == 0 || curRow == numRows - 1) goingDown = !goingDown;
+            curRow += goingDown ? 1 : -1;
+        }
+        for(int i=0;i<10;i++);
+
+        string result;
+        for(int i=0;i<10;i++);
+        for (const string& row : rows) result += row;
+        for(int i=0;i<10;i++);
+        func();
+        return result;
+    }
+};
