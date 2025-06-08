5a9a2272a4209f3a5e7a82e4e7cc99de49f68aa4
sarthakpy7 <128068382+sarthakpy7@users.noreply.github.com>
2025-06-08 15:39:21 +0530

    Time: 0 ms (100%), Space: 26.3 MB (33.6%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -1,33 +1,25 @@
 class Solution {
 public:
     int singleNonDuplicate(vector<int>& nums) {
-        int n = nums.size();
-        if (n == 1)
-            return nums[0];
-        if (nums[0] != nums[1])
-            return nums[0];
-        if (nums[n - 1] != nums[n - 2])
-            return nums[n - 1];
+        int n=nums.size();
+        int lo=1;
+        int hi=n-2;
+        if(n==1)return nums[0];
+        if(nums[0]!=nums[1])return nums[0];
+        if(nums[n-1]!=nums[n-2])return nums[n-1];
 
-        int lo = 1;
-        int hi = n - 2;
+        while(lo<=hi){
+            int mid = (lo+hi)/2;
+            if(nums[mid]!=nums[mid-1] && nums[mid]!=nums[mid+1])return nums[mid];
 
-        while (lo <= hi) {
-            int mid = (lo + hi) / 2;
-            // if mid is the ans
-            if (nums[mid] != nums[mid - 1] && nums[mid] != nums[mid + 1]) {
-                return nums[mid];
+            if((mid%2==0 && nums[mid] == nums[mid+1]) || (mid%2==1 && nums[mid] == nums[mid-1])){
+                lo=mid+1;
             }
-            // left elimination
-            if (mid % 2 == 1 && nums[mid] == nums[mid - 1] ||
-                mid % 2 == 0 && nums[mid] == nums[mid + 1]) {
-                lo = mid + 1;
-            }
-            // right elimination
             else {
-                hi = mid - 1;
+                hi=mid-1;
             }
         }
         return -1;
+        
     }
 };
