42709333eded171ecb7770211ebc2ef299341848
sarthakpy7 <128068382+sarthakpy7@users.noreply.github.com>
2025-06-08 11:55:18 +0530

    Time: 0 ms (100%), Space: 10.8 MB (92.42%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -13,7 +13,7 @@ public:
             maxFreq = max(maxFreq, freq[s[r] - 'A']);
 
             // If number of letters to change > k, move left pointer
-            while ((r - l + 1) - maxFreq > k) {
+            if ((r - l + 1) - maxFreq > k) {
                 freq[s[l] - 'A']--;
                 l++;
             }
