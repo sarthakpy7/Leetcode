840b31b18c66ede3eb60de9dc0e66b9325341602
sarthakpy7 <128068382+sarthakpy7@users.noreply.github.com>
2025-07-29 00:04:40 +0530

    Time: 7 ms (92.39%), Space: 10.5 MB (86.47%) - LeetHub

--- 2170-count-number-of-maximum-bitwise-or-subsets/2170-count-number-of-maximum-bitwise-or-subsets.cpp
+++ 2170-count-number-of-maximum-bitwise-or-subsets/2170-count-number-of-maximum-bitwise-or-subsets.cpp
@@ -1,33 +1,16 @@
-class Solution
-{
+class Solution {
 public:
-    void backtrack(const vector<int> &nums, int index, int currentOR, int maxOR, int &count)
-    {
-        if (currentOR == maxOR)
-        {
-            count++;
-        }
-
-        for (int i = index; i < nums.size(); ++i)
-        {
-            backtrack(nums, i + 1, currentOR | nums[i], maxOR, count);
-        }
+    int countMaxOrSubsets(vector<int>& nums) {
+        int maxOr = 0;
+        for (int n : nums) maxOr |= n;
+        return dfs(nums, 0, 0, maxOr);
     }
 
-    int countMaxOrSubsets(vector<int> &nums)
-    {
-        int maxOR = 0;
-
-        // Step 1: Compute the maximum OR
-        for (int num : nums)
-        {
-            maxOR |= num;
-        }
-
-        int count = 0;
-        // Step 2: Backtrack to count the subsets
-        backtrack(nums, 0, 0, maxOR, count);
-
-        return count;
+private:
+    int dfs(vector<int>& nums, int i, int currOr, int maxOr) {
+        if (i == nums.size()) return currOr == maxOr ? 1 : 0;
+        int include = dfs(nums, i + 1, currOr | nums[i], maxOr);
+        int exclude = dfs(nums, i + 1, currOr, maxOr);
+        return include + exclude;
     }
 };
