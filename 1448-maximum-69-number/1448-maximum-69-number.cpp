111ea03be9f8503b391772e28fae86bc1ef24520
sarthakpy7 <128068382+sarthakpy7@users.noreply.github.com>
2025-08-16 11:29:07 +0530

    Time: 0 ms (100%), Space: 7.9 MB (71.54%) - LeetHub

---
+++ 1448-maximum-69-number/1448-maximum-69-number.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int maximum69Number (int num) {
+        for (int i = 1000; i > 0; i /= 10) {
+            if ((num / i) % 10 == 6) {
+                num += 3 * i; // flip 6 → 9
+                break;
+            }
+        }
+        return num;
+    }
+};
