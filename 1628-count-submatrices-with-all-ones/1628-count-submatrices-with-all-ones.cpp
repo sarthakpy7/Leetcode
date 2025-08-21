942dfd3ac08fbaa2794a950267decac85e53f418
sarthakpy7 <128068382+sarthakpy7@users.noreply.github.com>
2025-08-21 12:41:40 +0530

    Time: 16 ms (38.96%), Space: 17.7 MB (100%) - LeetHub

---
+++ 1628-count-submatrices-with-all-ones/1628-count-submatrices-with-all-ones.cpp
@@ -0,0 +1,27 @@
+int st[150], top=-1;//mono stack
+int cnt[150]={0};
+class Solution {
+public:
+    static int numSubmat(vector<vector<int>>& mat) {
+        const int r=mat.size(), c=mat[0].size();
+ 
+        int ans=0;
+        for (int i=0; i<r; i++){
+            auto& h=mat[i];//height
+            top=-1;// reset mono stack
+            fill(cnt, cnt+c, 0);
+            for(int j=0; j<c; j++){
+                if (i>0 && h[j]>0) 
+                    h[j]+=mat[i-1][j];// height for mat[i][j]
+                
+                while(top>-1 && h[st[top]]> h[j]) 
+                    top--;// pop
+                int left=(top==-1)?-1:st[top];
+                cnt[j]=(top>-1?cnt[left]:0)+h[j]*(j-left);
+                ans+=cnt[j];
+                st[++top]=j;// push j to stack
+            }
+        }
+        return ans;
+    }
+};
