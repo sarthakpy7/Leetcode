d67915278512baff3180bf529ef33b9c534db6a9
sarthakpy7 <128068382+sarthakpy7@users.noreply.github.com>
2025-10-19 01:27:37 +0530

    Time: 129 ms (36.51%), Space: 97.7 MB (68.89%) - LeetHub

--- 3620-maximum-number-of-distinct-elements-after-operations/3620-maximum-number-of-distinct-elements-after-operations.cpp
+++ 3620-maximum-number-of-distinct-elements-after-operations/3620-maximum-number-of-distinct-elements-after-operations.cpp
@@ -1,20 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+
 class Solution {
 public:
     int maxDistinctElements(vector<int>& nums, int k) {
-        if (nums.empty()) return 0;
         sort(nums.begin(), nums.end());
-        int count = 0;
-        int prev = INT_MIN >> 1;
-        for (int a : nums) {
-            int low = a - k;
-            int high = a + k;
-            int x = prev + 1;
-            if (x < low) x = low;
-            if (x <= high) {
-                count++;
-                prev = x;
+        ll curr = LLONG_MIN;  // next smallest usable number
+        int ans = 0;
+        for (auto x : nums) {
+            ll start = x - k;
+            ll end = x + k;
+            if (max(start, curr) <= end) {
+                ans++;
+                curr = max(start, curr) + 1;  // pick smallest valid unique num
             }
         }
-        return count;
+        return ans;
     }
 };
