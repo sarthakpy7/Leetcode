0f200ca562dd5605079795bb32e5de43603260e1
sarthakpy7 <128068382+sarthakpy7@users.noreply.github.com>
2025-11-21 22:42:29 +0530

    Time: 24 ms (95.73%), Space: 12.7 MB (100%) - LeetHub

--- 2059-unique-length-3-palindromic-subsequences/2059-unique-length-3-palindromic-subsequences.cpp
+++ 2059-unique-length-3-palindromic-subsequences/2059-unique-length-3-palindromic-subsequences.cpp
@@ -1,27 +1,72 @@
+int n;
+int ll[26], rr[26];
 class Solution {
 public:
-    int countPalindromicSubsequence(string s) {
-        char c[26] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
-        int a = 0, t = 0;
-
-        int l, r;
-        for (char x : c) {
-            for (l = 0; l < s.length() && s[l] != x; l++);
-            if (l == s.length()) continue;
-            for (r = s.length() - 1; r >= 0 && s[r] != x; r--);
-            if (l >= r) continue;
-
-            vector<bool> v(128, false);
-            t = 0;
-            for (int i = l + 1; i < r; i++) {
-                if (!v[s[i]]) {
-                    v[s[i]] = true;
-                    t++;
-                    if (t == 26) break;
-                }
+    static inline void find_ll_rr(string& s){
+        fill(ll, ll+26, n);
+        memset(rr, -1, sizeof(rr));
+        
+        for(unsigned i=0; i<n; i++){
+            unsigned x=s[i]-'a';
+            if (ll[x]==n){
+                ll[x]=i;
             }
-            a += t;
+            rr[x]=i;
         }
-        return a;
+    }
+
+    static inline void find_ll_rr_2(string& s){
+        fill(ll, ll+26, n);
+        memset(rr, -1, sizeof(rr));
+    
+        int seen=0;
+        
+        for(unsigned i=0; i<n && seen<26; i++){
+            unsigned x=s[i]-'a';
+            if (ll[x]==n){
+                ll[x]=i;
+                seen++;
+            }
+        }
+    
+        for(unsigned i=n-1; i>=0 && seen>0; i--){
+            unsigned x=s[i]-'a';
+            if (rr[x]==-1){
+                rr[x]=i;
+                seen--;
+            }
+        }
+    }
+
+    static inline int hasPal3(char x,  string& s, int l, int r){
+        unsigned m;
+        bitset<26> seen=0;
+
+        for(m=l+1; m<r; m++){
+            seen[s[m]-'a']=1;
+        } 
+        return seen.count();
+    }
+
+    static int countPalindromicSubsequence(string& s) {
+        n=s.size();
+        find_ll_rr(s);
+        int count=0;
+    
+        for(char x='a'; x<='z'; x++){
+            int l=ll[x-'a'], r=rr[x-'a'];
+            if (l+1>=r) continue;
+            count+=hasPal3(x, s, l, r);
+        }   
+        return count;
     }
 };
+
+
+auto init = []()
+{ 
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    return 'c';
+}();
