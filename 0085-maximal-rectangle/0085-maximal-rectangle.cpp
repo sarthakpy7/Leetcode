3df250078937930931e5e66545290a6e3b40a5eb
sarthakpy7 <128068382+sarthakpy7@users.noreply.github.com>
2026-01-13 01:50:08 +0530

    Time: 37 ms (7.6%), Space: 17.2 MB (89.13%) - LeetHub

--- 0085-maximal-rectangle/0085-maximal-rectangle.cpp
+++ 0085-maximal-rectangle/0085-maximal-rectangle.cpp
@@ -1,59 +1,54 @@
 class Solution {
 public:
-    vector<int> rspc(const vector<int>& h, int n){
-        vector<int> ans(n);
-        stack<int> st;
-        st.push(-1);
-
-        for(int i=n-1; i>=0; i--){
-            int curr=h[i];
-            while(st.top()!=-1 && h[st.top()]>=curr) st.pop();
-            ans[i]=st.top();
-            st.push(i);
-        }
-        return ans;
-    }
-    vector<int> lspc(const vector<int>& h, int n){
-        vector<int> ans(n);
-        stack<int> st;
-        st.push(-1);
-
-        for(int i=0; i<n; i++){
-            int curr=h[i];
-            while(st.top()!=-1 && h[st.top()]>=curr) st.pop();
-            ans[i]=st.top();
-            st.push(i);
-        }
-        return ans;
-    }
-    int largestRectangle(const vector<int>& h, int n){
-        vector<int> ri(n);
-        vector<int> li(n);
-        ri = rspc(h, n);
-        li = lspc(h, n);
-        int maxarea=INT_MIN;
-        for(int i=0; i<n; i++){
-            int l=h[i];
-            if(ri[i]==-1) ri[i]=n;
-            int b = ri[i]-li[i]-1;
-            int area = l*b;
-            maxarea=max(maxarea, area);
-        }
-        return maxarea;
-    }
     int maximalRectangle(vector<vector<char>>& matrix) {
-        int n= matrix.size();
-        int m=matrix[0].size();
-        vector<int> hist(m, 0);
-        int ans =INT_MIN;
-        for(int i=0; i<n; i++){
-            for(int j=0; j<m; j++){
-                if(matrix[i][j]=='1') hist[j]++;
-                else hist[j]=0;
+        if (matrix.empty() || matrix[0].empty()) return 0;
+
+        int M = matrix.size();
+        int N = matrix[0].size();
+
+        // convert char to int (in-place)
+        vector<vector<int>> mat(M, vector<int>(N));
+        for (int i = 0; i < M; i++) {
+            for (int j = 0; j < N; j++) {
+                mat[i][j] = matrix[i][j] - '0';
             }
-            int area = largestRectangle(hist, m);
-            ans=max(ans, area);
         }
-        return ans;
+
+        // row-wise prefix widths
+        for (int i = 0; i < M; i++) {
+            for (int j = 1; j < N; j++) {
+                if (mat[i][j] == 1) {
+                    mat[i][j] += mat[i][j - 1];
+                }
+            }
+        }
+
+        int Ans = 0;
+
+        // fix each column
+        for (int j = 0; j < N; j++) {
+            for (int i = 0; i < M; i++) {
+                int width = mat[i][j];
+                if (width == 0) continue;
+
+                // expand downward
+                int currWidth = width;
+                for (int k = i; k < M && mat[k][j] > 0; k++) {
+                    currWidth = min(currWidth, mat[k][j]);
+                    int height = k - i + 1;
+                    Ans = max(Ans, currWidth * height);
+                }
+
+                // expand upward
+                currWidth = width;
+                for (int k = i; k >= 0 && mat[k][j] > 0; k--) {
+                    currWidth = min(currWidth, mat[k][j]);
+                    int height = i - k + 1;
+                    Ans = max(Ans, currWidth * height);
+                }
+            }
+        }
+
+        return Ans;
     }
 };
