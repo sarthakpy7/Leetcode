8b121775ba7783a53c3c0d9fc2f5868fe0438c60
sarthakpy7 <128068382+sarthakpy7@users.noreply.github.com>
2026-04-21 09:39:01 +0530

    Time: 0 ms (100%), Space: 28.8 MB (94.17%) - LeetHub

--- 1262-greatest-sum-divisible-by-three/1262-greatest-sum-divisible-by-three.cpp
+++ 1262-greatest-sum-divisible-by-three/1262-greatest-sum-divisible-by-three.cpp
@@ -1,38 +1,26 @@
 class Solution {
 public:
-    int maxSumDivThree(vector<int>& nums) {
-        int sum = 0;
-
-        int min1 = INT_MAX, min2 = INT_MAX;
-        int min11 = INT_MAX, min22 = INT_MAX;
+    static int maxSumDivThree(vector<int>& nums) {
+        const int inf=1e9;
+        int sum=0;
+        int a[2][2]={{inf, inf}, {inf , inf}};
 
         for (int x : nums) {
-            sum += x;
-            int r = x % 3;
-
-            if (r == 1) {
-                if (x < min1) { min11 = min1; min1 = x; }
-                else if (x < min11) min11 = x;
-            } else if (r == 2) {
-                if (x < min2) { min22 = min2; min2 = x; }
-                else if (x < min22) min22 = x;
-            }
+            sum+=x;
+            int m=x%3;
+            if (m) {
+                m--;
+                if (x<a[m][0]) a[m][1]=exchange(a[m][0], x);
+                else if (x<a[m][1]) a[m][1]=x;
+            } 
         }
 
-        int rem = sum % 3;
-
-        if (rem == 0) return sum;
-
-        if (rem == 1) {
-            int r1 = min1;
-            int r2 = (min2 == INT_MAX || min22 == INT_MAX) ? INT_MAX : min2 + min22;
-            int remove = min(r1, r2);
-            return remove == INT_MAX ? 0 : sum - remove;
-        } else {
-            int r1 = min2;
-            int r2 = (min1 == INT_MAX || min11 == INT_MAX) ? INT_MAX : min1 + min11;
-            int remove = min(r1, r2);
-            return remove == INT_MAX ? 0 : sum - remove;
-        }
+        int r=sum%3, remove=0;
+        if (r) {
+            r--;
+            remove=min(a[r][0], a[r^1][0]+a[r^1][1]);
+            (remove<inf)?sum-=remove:sum=0;
+        } 
+        return sum;
     }
 };
