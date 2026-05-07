a8e1f2c684180c001431b9df9debcc80623d0e56
sarthakpy7 <128068382+sarthakpy7@users.noreply.github.com>
2026-05-07 11:04:42 +0530

    Time: 0 ms (100%), Space: 92.9 MB (36.68%) - LeetHub

--- 3577-count-the-number-of-computer-unlocking-permutations/3577-count-the-number-of-computer-unlocking-permutations.cpp
+++ 3577-count-the-number-of-computer-unlocking-permutations/3577-count-the-number-of-computer-unlocking-permutations.cpp
@@ -1,20 +1,19 @@
 class Solution {
 public:
-    static const int MOD = 1000000007;
-
     int countPermutations(vector<int>& complexity) {
+        const long long mod = 1000000007;
         int n = complexity.size();
-        int first = complexity[0];
 
         for (int i = 1; i < n; i++) {
-            if (complexity[i] <= first) return 0;
+            if (complexity[i] <= complexity[0])
+                return 0;
         }
 
-        long long fact = 1;
-        for (int i = 2; i < n; i++) {
-            fact = (fact * i) % MOD;
+        long long ans = 1;
+        for (int i = 1; i < n; i++) {
+            ans = (ans * i) % mod;
         }
 
-        return (int)fact;
+        return (int)ans;
     }
 };
